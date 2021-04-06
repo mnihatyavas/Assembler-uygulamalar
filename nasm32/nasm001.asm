755887e34d55b6441ac6a13b5c14ee8d1471cff3
M.Nihat Yavaş <46388635+mnihatyavas@users.noreply.github.com>
2021-04-06 05:17:04 +0300

    Add files via upload

---
+++ nasm32/nasm001.asm
@@ -0,0 +1,16 @@
+; nasm001.asm: Merhaba dünya mesajýnýn dos penceresinden yansýtýlmasý örneði.
+; ----------------------------------------------------------------------------
+    global _main
+    extern _printf ; C _printf fonksiyonunu çaðýrýr...
+
+    section .text
+_main:
+    push mesaj
+    call _printf
+    add esp, 4
+    ret ; return
+mesaj:
+    db "Merhaba, Assembler Dünyasý!", 10, 0
+; ----------------------------------------------------------------------------
+; nasm -fwin32 nasm001.asm
+; gcc -m32 nasm001.obj -o nasm001.exe
